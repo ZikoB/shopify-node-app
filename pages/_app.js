75e904397e5cf73c32a8e50af91b060236bda579
Zhivko Bogoevski <zhivkobogoevski@Zhivkos-MacBook-Pro.local>
2019-07-03 21:29:33 -0700

    billing api, test recurring charge

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,36 @@
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "react-apollo";
-import App, { Container } from "next/app";
+import App from "next/app";
+import Head from "next/head";
 import { AppProvider } from "@shopify/polaris";
 import { Provider } from "@shopify/app-bridge-react";
 import "@shopify/polaris/styles.css";
+import Cookies from "js-cookie";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
 const client = new ApolloClient({
   fetchOptions: {
     credentials: "include",
   },
 });
+
 class MyApp extends App {
+  // state = {
+  //   shopOrigin: Cookies.get("shopOrigin"),
+  // };
+
   static async getInitialProps(server) {
     const shopOrigin = server.ctx.query.shop;
     return { shopOrigin };
   }
+
   render() {
     const { Component, pageProps, shopOrigin } = this.props;
     return (
-      <Container>
+      <React.Fragment>
+        <Head>
+          <title>Sample App</title>
+          <meta charSet="utf-8" />
+        </Head>
         <AppProvider>
           <Provider
             config={{
@@ -32,7 +44,7 @@ class MyApp extends App {
             </ApolloProvider>
           </Provider>
         </AppProvider>
-      </Container>
+      </React.Fragment>
     );
   }
 }
